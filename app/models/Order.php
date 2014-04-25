d1c9ee5af8455e09fdea63a5238f9784b1f678a3
VytasJ <vycka2004lt@gmail.com>
2014-04-25 14:05:19 +0300

    added order rules

--- app/models/Order.php
+++ app/models/Order.php
@@ -9,14 +9,24 @@ class Order extends Eloquent {
     public static $rules = [
         'data' => 'required',
         'kiekis' => 'required|numeric|max:32000|min:1',
+        'kaina' => 'required|numeric|max:99999999.99|min:0',
+        'nuolaida' => 'required|numeric|max:100|min:0',
+        'daktaras_id' => 'required',
+        'produktas_id' => 'required'
 
     ];
     public static $messages = [
         'data.required' => 'Data: Neįvesti duomenys',
         'kiekis.numeric' => 'Kiekis gali būti tik skaičiai.',
         'kiekis.required' => 'Kiekis: Neįvesti duomenys.',
-        'kiekis.min' => 'Kiekiss negali būti mažiau nei 1.',
+        'kiekis.min' => 'Kiekis negali būti mažiau nei 1.',
         'kiekis.max' => 'Kiekis negali būti daugiau už 32000.',
+        'kaina.min' => 'Kaina negali būti mažiau nei 0.',
+        'kaina.max' => 'Kaina negali būti daugiau už 99999999.99.',
+        'nuolaida.min' => 'Kiekis negali būti mažiau nei 1.',
+        'nuolaida.max' => 'Kiekis negali būti daugiau už 100.',
+        'daktaras_id.required' => 'Iš sąrašo nepasirinktas klientas',
+        'produktas_id.required' => 'Iš sąrašo nepasirinktas produktas'
 
     ];
 
