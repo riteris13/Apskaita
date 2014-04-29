a5c3a63833eadb31171a790b59ce97416436ab7d
VytasJ <vycka2004lt@gmail.com>
2014-04-29 13:47:48 +0300

    added missing td

--- public/js/order.js
+++ public/js/order.js
@@ -1,16 +1,18 @@
 function getDetails(id){
 var bendraSuma = 0;
 	$.getJSON("order/details", {option: id}, 
-			function(data) {
+			function(data) {	
 			$('#modalTable').empty();
 			$.each(data, function(index, element) {
 			var suma = element.pir_kaina * element.kiekis;
 			bendraSuma += suma;
 				$('#modalTable').append(
-				'<tr>' + '<td>' + element.produktas + '</td>' +
-					'<td>' + element.pir_kaina + '</td>' + '<td>' + element.kiekis + 
-					'<td>' + suma.toFixed(2) + '</tr>');
-
+				'<tr>' + 
+					'<td>' + element.produktas + '</td>' +
+					'<td>' + element.pir_kaina + '</td>' + 
+					'<td>' + element.kiekis + '</td>' + 
+					'<td>' + suma.toFixed(2) + '</td>' + 
+				'</tr>');
 			});
 			document.getElementById('bendra_suma').innerHTML =  "Bendra pardavimo suma: " + bendraSuma.toFixed(2) + " LTL";
 		});	
