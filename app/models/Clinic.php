954bff0d44ee4a4f72b3eeaa23c8a26c5510a6ae
VytasJ <vycka2004lt@gmail.com>
2014-05-13 15:10:48 +0300

    clinic validation fix

--- app/models/Clinic.php
+++ app/models/Clinic.php
@@ -9,14 +9,15 @@ class Clinic extends Eloquent {
     }
     public static $rules = [
         'pavadinimas' => 'required|alpha_num_spaces|max:45',
-        'adresas' => 'required|max:128',
+        'adresas' => 'required|alpha_num_spaces|max:128',
         'kodas' => 'required|numeric|regex:/^[0-9]{9}$/'
     ];
     public static $messages = [
         'pavadinimas.required' => 'Pavadinimas: Neįvesti duomenys.',
         'adresas.required' => 'Adresas: Neįvesti duomenys.',
         'kodas.required' => 'Įmonės kodas: Neįvesti duomenys.',
-        'pavadinimas.alpha_num_spaces' => 'Pavadinimas: Gali būti naudojamos tik raidės ir skaičiai, be tarpų.',
+        'pavadinimas.alpha_num_spaces' => 'Pavadinimas: Gali būti naudojamos tik raidės ir skaičiai su tarpais.',
+        'adresas.alpha_num_spaces' => 'Adresas: Gali būti naudojamos tik raidės ir skaičiai su tarpais.',
         'pavadinimas.max' => 'Pavadinimas: Įvestas tekstas negali būti ilgesnis nei 45 simboliai.',
         'adresas.max' => 'Adresas: Įvestas tekstas negali būti ilgesnis nei 128 simboliai.',
         'kodas.regex' => 'Kodas: Įvestas tekstas negali būti trumpesnis arba ilgesnis nei 9 simboliai.',
