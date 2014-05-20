2807590851ad32720e4e9f1d09c1e86568a92437
VytasJ <vycka2004lt@gmail.com>
2014-05-21 00:48:16 +0300

    js fix

--- public/js/salesReport.js
+++ public/js/salesReport.js
@@ -1,6 +1,6 @@
 function calculateTotal(bSuma, tarpSum){
 		for(j=1; j<=tarpSum.length-1; j++){
-			myTable = document.getElementById('tblExport');
+			myTable = document.getElementById('tblSales');
 			myTable.rows[j].cells[4].innerHTML = (100/bSuma*tarpSum[j]).toFixed(2) + "%";
 		}
 }
