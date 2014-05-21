fe6e7f5aec6b646a37e28beac843f9f150c51423
VytasJ <vycka2004lt@gmail.com>
2014-05-22 00:51:53 +0300

    sales report css

--- app/views/report/salesReport.blade.php
+++ app/views/report/salesReport.blade.php
@@ -3,9 +3,48 @@
 <?php $i=0;?>
 @section('content')
 <script src="/js/salesReport.js"></script>
+<style>
+    .rinkos,
+    .dol,
+    .amount,
+    .ltl,
+    .names,
+    .dollar{
+        height: 16px;
+        padding: 0px 0px;
+        font-size: 13px;
+        line-height: 0px;
+        border: 0px;
+        box-shadow: 0px;
+        outline: 0 none;
+    }
+    .ltl,
+    .dol{
+        text-align: right;
+        width: 60%;
+    }
+    .amount,
+    .rinkos{
+        text-align: center;
+        width: 100%;
+    }
+    .names{
+        text-align: left;
+        width: 100%;
+    }
+    .dollar{
+        text-align: left;
+        width: 10%;
+    }
+    .table-condensed>tbody>tr>td {
+        padding: 0px;
+        height: 15px;
+        font-size: 13px;
+    }
+</style>
 
 {{ Form::open(array('url' => 'export/sales')) }}
-{{ "Doleris:", Form::text('Doleris', 2.5031,  array('id'=>'Dol_val'))}}
+{{ "Dolerio kursas: ", Form::text('Doleris', 2.5031,  array('id'=>'Dol_val', 'class'=>'dollar'))}}
 <br><br>
 <table class="table table-bordered" id="tblSales">    <!-- max table dydziui style="width:842px" -->
     <thead style="font-weight: bold; text-align: center;">
@@ -24,7 +63,7 @@
             {{{++$i}}}
         </td>
         <td class="text-left">
-            {{ Form::text('name[]', $item->pavadinimas)}}
+            {{ Form::text('name[]', $item->pavadinimas, array('class'=>'names'))}}
         </td>
         <td>
             <?php $kiekis=0; $suma=0;?>
