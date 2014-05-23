b1729a7e59a90c9a28fc5882dcd8ba6c056c05d8
Vladimiras <vladimirasdes@gmail.com>
2014-05-23 21:45:46 +0300

    dns change

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'riteris13.nacija.lt',
+			'host'      => 'riteris13.etchost.lt',
 			'database'  => 'apskaita',
 			'username'  => 'Moderator',
 			'password'  => 'fuw4bruT',
