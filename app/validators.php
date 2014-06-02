6f79a1513a32dab0d0af9691868c3bb7c927f6ef
VytasJ <vycka2004lt@gmail.com>
2014-06-02 10:27:23 +0300

    LT raides

--- app/validators.php
+++ app/validators.php
@@ -5,5 +5,5 @@ return preg_match('/^[\pL\s]+$/u', $value);
 });
 Validator::extend('alpha_num_spaces', function($attribute, $value)
 {
-    return preg_match('/^([-a-z0-9_-\s])+$/i', $value);
+    return preg_match('/^[\pL\pN_-\s]+$/u', $value);
 });
